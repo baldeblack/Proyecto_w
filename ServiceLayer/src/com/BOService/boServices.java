00cd6f390882b53b0e1df04c8b085ce92e43a93a
sandinof <sandinoaf@gmail.com>
2015-02-01 11:09:33 -0200

    servivios jsonp

--- ServiceLayer/src/com/BOService/boServices.java
+++ ServiceLayer/src/com/BOService/boServices.java
@@ -5,6 +5,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -33,20 +34,22 @@ public class boServices {
 //		
 //	}
 	
-	@POST
+	@GET
+	//@Produces("application/javascript")
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("login/{input}")
-	public Response ExisteUsu(@PathParam("input") String input){
+	@Path("/login/{mail}/{pass}")
+	public String ExisteUsu(@PathParam("mail") String mail, @PathParam("pass") String pass, @QueryParam("callback") String callback){
 		
-		String response = null;
+		String response = "hola";
 		ICUsuarios iu = new CUsuarios();
 		Gson g = new Gson();
-		login l = g.fromJson(input, login.class);
-		Usuario u  = iu.Login(l.mail, l.pass);
-		
+		//login l = g.fromJson(input, login.class);
+		Usuario u  = iu.Login(mail, pass);
+//		Gson g = new Gson();
 		response = g.toJson(u);
-		return Response.ok(response).build();
-
+		//Response.ok().header("Access-Control-Allow-Origin", "*");
+		//return Response.ok(response).build();
+		 return (callback + "(" + response + ")");
 		
 	}
 	
@@ -55,4 +58,22 @@ public class boServices {
 		public String pass;
 	}
 	
+	@GET
+	//@Produces("application/javascript")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/pasos/{pasos}")
+	public String listPasos(@PathParam("pasos") String pasos, @QueryParam("callback") String callback){
+		
+		String response = "hola";
+		ICUsuarios iu = new CUsuarios();
+		Gson g = new Gson();
+		//login l = g.fromJson(input, login.class);
+		//Usuario u  = iu.Login(mail, pass);
+//		Gson g = new Gson();
+		//response = g.toJson(u);
+		//Response.ok().header("Access-Control-Allow-Origin", "*");
+		//return Response.ok(response).build();
+		 return (callback + "(" + response + ")");
+		
+	}
 }
